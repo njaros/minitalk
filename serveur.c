792b6019a1a15f584852b9c375e26a12494be5c2
Nicolas Jaros <njaros@z4r10p7.42lyon.fr>
2022-01-11 16:36:13 +0100

    ajout du static char* pour correspondre au sujet

--- serveur.c
+++ serveur.c
@@ -13,10 +13,36 @@
 #include "minitalk.h"
 #include <sys/errno.h>
 
+char	*ajout_char(char *str, char c, int reinit)
+{
+	int			j;
+	static int	i = 1;
+	char		*charade;
+
+	if (reinit == 1)
+	{
+		i = 1;
+		free(str);
+		return (NULL);
+	}
+	j = -1;
+	charade = malloc(i + 1);
+	if (str)
+	{
+		while (str[++j])
+			charade[j] = str[j];
+	}
+	charade[++i - 2] = c;
+	charade[i - 1] = '\0';
+	free(str);
+	return (charade);
+}
+
 void	handler(int sig, siginfo_t *siginfo, void *ucontext)
 {
 	static char	c = 0x00;
 	static int	bit_pos = 0;
+	static char	*str = NULL;
 
 	(void) ucontext;
 	if (sig == SIGUSR2)
@@ -24,7 +50,13 @@ void	handler(int sig, siginfo_t *siginfo, void *ucontext)
 	bit_pos++;
 	if (bit_pos >= 8)
 	{
-		ft_putchar_fd(c, 1);
+		if (c)
+			str = ajout_char(str, c, 0);
+		else
+		{
+			ft_putstr_fd(str, 1);
+			str = ajout_char(str, c, 1);
+		}
 		bit_pos = 0;
 		c = 0x00;
 	}
@@ -44,7 +76,7 @@ int	main(void)
 	sigaddset(&act.sa_mask, SIGINT);
 	sigaddset(&act.sa_mask, SIGKILL);
 	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = handler;
+	act.__sigaction_u.__sa_sigaction = handler;
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
 	pid = getpid();
